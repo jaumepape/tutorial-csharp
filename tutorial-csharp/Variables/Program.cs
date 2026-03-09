3525a9712a80b2618d2846f4cc08210ab10a53b0
jaumepape <jaume@vdata.dev>
2026-03-09 12:37:02 +0100

    Variables 1

---
+++ tutorial-csharp/Variables/Program.cs
@@ -0,0 +1,17 @@
+﻿var entero = 1;
+int entero2 = 2;
+var preu = 1000d;
+double preu2 = 2000;
+var nom = "Jaume";
+string nom2 = "Jordi";
+var verdadero = true;
+bool falso = false;
+var lletra = 'a';
+char lletra2 = 'b';
+
+Console.WriteLine($"El meu nom és: {nom}");
+Console.WriteLine($"El nom del meu germà és: {nom2}");
+Console.WriteLine($"{entero} {entero2}");
+Console.WriteLine($"{preu} {preu2}");
+Console.WriteLine($"{nom} {nom2}");
+Console.WriteLine($"{verdadero} {falso}");
