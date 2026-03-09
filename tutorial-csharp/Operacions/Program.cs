d59eef8e374d8ad0d2fe3ce06f85d4d122638042
jaumepape <jaume@vdata.dev>
2026-03-09 12:45:04 +0100

    Operaciones

---
+++ tutorial-csharp/Operacions/Program.cs
@@ -0,0 +1,20 @@
+﻿var a = 5;
+var b = 6;
+
+Console.WriteLine($"{a + b}");
+Console.WriteLine($"{a - b}");
+Console.WriteLine($"{a * b}");
+Console.WriteLine($"{a / b}");
+Console.WriteLine($"{a % b}");
+
+var suma = a + b;
+var resta = a - b;
+var multiplicacion = a * b;
+var division = a / b;
+var porcentaje = a % b;
+
+Console.WriteLine($"{suma}");
+Console.WriteLine($"{resta}");
+Console.WriteLine($"{multiplicacion}");
+Console.WriteLine($"{division}");
+Console.WriteLine($"{porcentaje}");
