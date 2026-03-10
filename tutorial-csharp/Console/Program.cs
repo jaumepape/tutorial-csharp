0c47b37d3aa06a7b9acb49af32c692377122ff32
jaumepape <jaume@vdata.dev>
2026-03-10 12:52:40 +0100

    Tema 0 partes 1-4

---
+++ tutorial-csharp/Console/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Hola ");
+Console.Write("mundo");
+Console.WriteLine("!");
+
+Console.WriteLine("Segunda linea");
+Console.WriteLine();
+Console.WriteLine("Cuarta linea");
+
+Console.WriteLine("Nombre\t\tEdad\tCiudad");
+Console.WriteLine("------\t\t----\t------");
+Console.WriteLine("Jaume\t\t18\tTarragona");
+Console.WriteLine("Elizabeth\t20\tBilbao");
+
+Console.WriteLine("Ruta: C:\\Usuarios\\Documentos");
+Console.WriteLine("Dijo: \"Hola mundo\"");
+Console.WriteLine("Linea 1\nLinea 2\nLinea 3");
+Console.WriteLine("Col1\tCol2\tCol3");
+Console.WriteLine("100\u20AC");
+
+Console.ForegroundColor = ConsoleColor.Green; 
+Console.WriteLine("Operación completada con éxito");
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Error: archivo no encontrado");
+
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("   TÍTULO DEL PROGRAMA   ");
+
+Console.ResetColor();
+Console.WriteLine("Texto normal de nuevo");
